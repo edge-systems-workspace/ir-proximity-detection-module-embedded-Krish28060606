dc676bfec2fdddeee3879612b9d5c7eb2471b488
Krish28060606 <krish.dubey_cs.aiml24@gla.ac.in>
2026-02-18 15:27:52 +0530

    Implement setup function
    Updated author name and added setup function for initialization.

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,23 @@
  * and a message is printed to the Serial Monitor.
  * When no object is detected, the LED turns OFF.
  *
- * @author Bhartendu Ji
+ * @author Krish dubey
  * @date 18 Feb 2026
  */
 // Pin Definitions
 int irPin = 7;     ///< IR sensor OUT pin connected to digital pin 7
 int ledPin = 13;   ///< LED connected to digital pin 13
+
+/**
+ * @brief Initializes serial communication and pin configuration.
+ *
+ * This function runs once when the Arduino starts.
+ * - Sets IR sensor pin as INPUT
+ * - Sets LED pin as OUTPUT
+ * - Starts Serial communication at 9600 baud rate
+ */
+void setup() {
+  Serial.begin(9600);
+  pinMode(irPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
